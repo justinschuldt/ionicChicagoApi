48b1aeaa6ea9a6de2044a3d0069982b7e9962ccd
Justin Schuldt <justinschuldt@gmail.com>
2016-05-12 21:05:49 -0500

    promsie bugfix

--- api/getImages.js
+++ api/getImages.js
@@ -4,26 +4,34 @@ module.exports = {
     get: function (req, res, next) {
         var imageTable = req.azureMobile.tables('images');
         var promiseArr = []
-        imageTable.where().read().then(results => {
+        imageTable.where({}).read().then(results => {
+            console.log('table results ', results);
             results.forEach(item => {
-                var query = {
-                    sql: 'select t.tag, t.id from images_tags it left join tags t on t.id = it.tagsId where it.imagesId = @imagesId',
-                    parameters: [
-                        { name: 'imagesId', value: item.id }
-                    ]
-                };
-                promiseArr.push(req.azureMobile.data.execute(query)
-                    .then(function (results) {
-                        item.tags = results;
-                        Promise.resolve(item);
-                }, error => Promise.reject(error)))
+                var prom = new Promise(function(resolve, reject){
+                    var query = {
+                        sql: 'select t.tag, t.id from images_tags it left join tags t on t.id = it.tagsId where it.imagesId = @imagesId',
+                        parameters: [
+                            { name: 'imagesId', value: item.id }
+                        ]
+                    };
+                    console.log('sql: ', query);
+                    req.azureMobile.data.execute(query)
+                        .then(function (results) {
+                            console.log(results)
+                            item.tags = results;
+                            resolve(item);
+                    }, error => reject(error))
+                })
+                promiseArr.push(prom);
+
                 
             })
+            Promise.all(promiseArr).then(result => {
+                res.status(200).send(result);
+            }, error => res.status(500).send(error))
         })
 
             
-        Promise.all(promiseArr).then(result => {
-            res.status(200).send(result);
-        }, error => res.status(500).send(error))
+
     }
 }
