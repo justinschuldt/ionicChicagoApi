5cf4121cfab9352a127fdaab95800f3f60633b29
Justin Schuldt <justinschuldt@gmail.com>
2016-05-16 09:35:47 -0500

    tags table read debugging

--- tables/tags.js
+++ tables/tags.js
@@ -3,4 +3,10 @@ var azureMobileApps = require('azure-mobile-apps');
 // Create a new table definition
 var table = azureMobileApps.table();
 
+table.read(function (context) {
+    console.log('read tags table');
+    return context.execute();
+});
+
+
 module.exports = table;
